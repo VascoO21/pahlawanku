e327adf2170c870d2878ff2b7d7ab45893b8692c
VascoO21 <114376262+VascoO21@users.noreply.github.com>
2022-11-14 11:11:45 +0700

    Pahlawanku

---
+++ app/src/main/java/pab1/if3B/pahlawanku/AdapterCard.java
@@ -0,0 +1,22 @@
+package pab1.if3B.pahlawanku;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterCard {
+    public class classViewHolder extends RecyclerView.ViewHolder{
+        ImageView ivFoto;
+        TextView tvNama, tvTentang;
+        public classViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFoto = itemView.findViewById(R.id.iv_foto);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvTentang = itemView.findViewById(R.id.tv_tentang);
+            
+        }
+    }
+}
